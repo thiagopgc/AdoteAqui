345fa6f7908c00a13c1b150bc6d679b9e57e6c9e
tiagopgc <thiagopereiragc@yahoo.com.br>
2021-11-13 14:29:45 -0300

    commit do thiago

---
+++ feira.html/comparativo.js
@@ -0,0 +1,22 @@
+var nomeAlimentos = prompt('Qual e o alimento')
+
+var valorAnterior = prompt('Qual era o valor ha uma semana atras')
+
+var valorAtual = prompt('Qual valor atual do alimento')
+
+
+if(nomeAlimentos=="tomate" || 
+nomeAlimentos =="laranja" ||
+nomeAlimentos == "limão" ||
+nomeAlimentos == "abacaxi"
+) {
+    if(valorAtual > valorAnterior){
+        console.log('O Alimento' + nomeAlimentos + 'Aumentou em '+)
+    }
+
+
+
+
+
+
+}
